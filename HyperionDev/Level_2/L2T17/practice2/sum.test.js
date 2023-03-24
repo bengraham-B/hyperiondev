f925d3b1a374a9695421325c1e4c9810de237196
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-03-25 00:10:08 +0200

    Routine Uplaod

--- HyperionDev/Level_2/L2T17/practice2/sum.test.js
+++ HyperionDev/Level_2/L2T17/practice2/sum.test.js
@@ -6,3 +6,13 @@ test('properly adds two numbers', () => {
         sum(1, 2)
     ).toBe(3)
 })
+
+
+test('renders the landing page', () => {
+  render(<App />);
+  
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+  expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
+  expect(screen.getByRole("img")).toBeInTheDocument();
+});
